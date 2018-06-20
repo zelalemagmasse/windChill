692962e5a088a761c6eaf2976613da18c8bc262f
zelalemagmasse <zelalem.agmasse@gmail.com>
2018-06-20 12:12:58 -0400

    updated version

--- windSpeedCalcu.js
+++ windSpeedCalcu.js
@@ -2,8 +2,8 @@ function Calculate() {
     var TempInFahr = document.getElementById("tempInFahr").value;
     var WindSpeed = document.getElementById("windSpeed").value;
 
-    var windChill = 35.74 + 0.6215*TempInFahr + (0.4275*TempInFahr - 35.75)  *  WindSpeed ^ 0.16;
+    var windChill = 35.74 + 0.6215*TempInFahr + (0.4275*TempInFahr - 35.75)  *  Math.pow(WindSpeed ,0.16);
 
 
-    document.getElementById("calculate").innerHTML = windChill;
+    document.getElementById("result").value = windChill;
 }
